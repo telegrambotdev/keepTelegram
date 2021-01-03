daac8d624300b9879f8e839e6b5d69fa2f2997f5
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-03 21:18:54 +0200

    #2 Add tests for Telegram

--- test/utest_main.py
+++ test/utest_main.py
@@ -1,14 +1,22 @@
+import os
+import requests
 import unittest
+
 from bot import dbWorker
 
 
-class Path:
-    """Parent class with the common field"""
+class Parent:
+    """Parent class with need variables"""
     path = './database.db'
+    token = os.getenv('TOKEN')
+    url = f'https://api.telegram.org/bot{token}/'
+    ok_status_code = 200
+    bot_name = 'Telegram Keep'
 
 
-class DBTest1(unittest.TestCase, Path):
+class DBTest1(unittest.TestCase, Parent):
     """First class for testing work with database"""
+
     def test_create(self):
         """Test creation of database"""
         self.assertTrue(dbWorker.create_db(self.path))
@@ -19,8 +27,9 @@ class DBTest1(unittest.TestCase, Path):
         self.assertIsNotNone(dbWorker.connect(self.path))
 
 
-class DBTest2(unittest.TestCase, Path):
+class DBTest2(unittest.TestCase, Parent):
     """First class for testing work with database"""
+
     def test_add(self):
         """Test adding data to database"""
         conn = dbWorker.connect(self.path)
@@ -37,3 +46,18 @@ class DBTest2(unittest.TestCase, Path):
         self.assertListEqual(dbWorker.get_notes(cursor, data),
                              [('test_id', 'test_header', 'test_text', 1, 'test_time')])
         self.assertListEqual(dbWorker.get_notes(cursor, ''), [])
+
+
+class TelegramTest(unittest.TestCase, Parent):
+    def setUp(self):
+        self.getMe = requests.get(self.url + 'getMe')
+
+    def test_conn(self):
+        self.assertEqual(self.getMe.status_code, self.ok_status_code)
+
+    def test_fields(self):
+        self.assertTrue(self.getMe.json()["ok"])
+        self.assertTrue(self.getMe.json()["result"]["is_bot"])
+        self.assertFalse(self.getMe.json()["result"]["can_read_all_group_messages"])
+        self.assertFalse(self.getMe.json()["result"]["supports_inline_queries"])
+        self.assertEqual(self.getMe.json()["result"]["first_name"], self.bot_name)
