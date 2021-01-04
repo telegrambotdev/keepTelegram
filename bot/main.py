e94e519116f307a64fbabff4659e656f31d4263f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-04 18:32:11 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 from bot import dbWorker
 import os
+import time
 import telebot
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,23 +18,28 @@ def send_welcome(message):
 def add_note(message):
     chat_id = message.chat.id
     data = message.text.split('\n')[1:]
+    timestamp = time.time()
     print(data)
     if len(data) == 3:
         conn = dbWorker.connect(os.getenv("DB_PATH"))
-        dbWorker.add_note(
-            conn, conn.cursor(), [
-                chat_id, data[0], data[1], 0, data[2]])
-        conn.close()
-        bot.send_message(chat_id, "good")
+        note = dbWorker.get(conn.cursor(), 'id', timestamp)
+        if not note:
+            dbWorker.add(
+                conn, conn.cursor(), [
+                    chat_id, data[0], data[1], 0, data[2], timestamp])
+            conn.close()
+            bot.send_message(chat_id, f'Your note with the header \"{data[0]}\" has been added')
+        else:
+            bot.send_message(chat_id, f'Note with the header \"{data[0]}\" exists')
     else:
-        bot.send_message(chat_id, "bad")
+        bot.send_message(chat_id, 'Please, write you message correctly')
     print('Closing connection is successful')
 
 
 @bot.message_handler(commands=['get'])
 def get_notes(message):
     conn = dbWorker.connect(os.getenv("DB_PATH"))
-    data = dbWorker.get_notes(conn.cursor(), 'test_id')
+    data = dbWorker.get(conn.cursor(), 'test_id')
     bot.reply_to(message, "Hello, how are you?")
     conn.close()
     print('Data is: ', data)
