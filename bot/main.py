eb8535ce244a6dd7def4f7e9aef305ae78557266
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-04 17:46:36 +0200

    Get data from message

--- bot/main.py
+++ bot/main.py
@@ -16,10 +16,15 @@ def send_welcome(message):
 @bot.message_handler(commands=['add'])
 def add_note(message):
     conn = dbWorker.connect(os.getenv("DB_PATH"))
-    dbWorker.add_note(
-        conn, conn.cursor(), [
-            'test_id', 'test_header', 'test_text', 1, 'test_time'])
-    bot.reply_to(message, "Hello, how are you?")
+    chat_id = message.chat.id
+    print(message.text.split('\n'))
+    if len(message.text.split('\n')) == 2:
+        bot.send_message(chat_id, "good")
+    else:
+        bot.send_message(chat_id, "bad")
+    # dbWorker.add_note(
+    #     conn, conn.cursor(), [
+    #         chat_id, 'test_header', 'test_text', 0, 'test_time'])
     conn.close()
     print('Closing connection is successful')
 
