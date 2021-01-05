1b8135caf274b2b092654046776e51380431522a
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-05 09:32:36 +0200

    Improve get function

--- bot/main.py
+++ bot/main.py
@@ -3,15 +3,27 @@ import os
 import time
 import telebot
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 bot = telebot.TeleBot(os.getenv("TOKEN"))
 
 
+# ------------ Program functions start ------------ #
+def note_template(data):
+    return f"""
+<strong>Header</strong>: <i>{data[1]}</i>
+<strong>Text</strong>: <i>{data[2]}</i>
+<strong>Status</strong>: <i>{data[3]}</i>
+<strong>Due time</strong>: <i>{data[4]}</i>
+"""
+# ------------ Program functions end ------------ #
+
+
+# ------------ Bot functions start ------------ #
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello, how are you?")
+    bot.reply_to(message, 'Hello, how are you?')
 
 
 @bot.message_handler(commands=['add'])
@@ -21,7 +33,7 @@ def add_note(message):
     timestamp = time.time()
     print(data)
     if len(data) == 3:
-        conn = dbWorker.connect(os.getenv("DB_PATH"))
+        conn = dbWorker.connect(os.getenv('DB_PATH'))
         note = dbWorker.get(conn.cursor(), 'id', timestamp)
         if not note:
             dbWorker.add(
@@ -38,17 +50,22 @@ def add_note(message):
 
 @bot.message_handler(commands=['get'])
 def get_notes(message):
-    conn = dbWorker.connect(os.getenv("DB_PATH"))
-    data = dbWorker.get(conn.cursor(), 'test_id')
-    bot.reply_to(message, "Hello, how are you?")
+    bot.reply_to(message, 'Your notes:')
+    conn = dbWorker.connect(os.getenv('DB_PATH'))
+    chat_id = message.chat.id
+    data = dbWorker.get(conn.cursor(), 'chat_id', chat_id)
+    for note in data:
+        msg = note_template(note)
+        bot.send_message(chat_id, msg, parse_mode='HTML')
     conn.close()
-    print('Data is: ', data)
     print('Closing connection is successful')
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
+# ------------ Bot functions end ------------ #
 
 
 print("Bot started")
