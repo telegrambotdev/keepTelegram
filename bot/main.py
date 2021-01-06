81ea9968f0b59fbad0f9ccfed9a6ce6bde129a6a
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-06 09:18:56 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,11 +5,12 @@ import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# ------------ Program variable start ----------- #
 bot = telebot.TeleBot(os.getenv("TOKEN"))
+# ------------ Program variables end ------------ #
 
 
-# ------------ Program functions start ------------ #
+# ------------ Program functions start ---------- #
 def note_template(data):
     return f"""
 <strong>Header</strong>: <i>{data[1]}</i>
@@ -20,7 +21,7 @@ def note_template(data):
 # ------------ Program functions end ------------ #
 
 
-# ------------ Bot functions start ------------ #
+# ------------ Bot functions start ---------- #
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
     bot.reply_to(message, 'Hello, how are you?')
@@ -28,18 +29,17 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['add'])
 def add_note(message):
+    SQLighter = dbWorker.SQLighter(os.getenv('DB_PATH'))
     chat_id = message.chat.id
     data = message.text.split('\n')[1:]
     timestamp = time.time()
     print(data)
     if len(data) == 3:
-        conn = dbWorker.connect(os.getenv('DB_PATH'))
-        note = dbWorker.get(conn.cursor(), 'id', timestamp)
+        note = SQLighter.get('id', timestamp)
         if not note:
-            dbWorker.add(
-                conn, conn.cursor(), [
+            SQLighter.add([
                     chat_id, data[0], data[1], 0, data[2], timestamp])
-            conn.close()
+            SQLighter.close()
             bot.send_message(chat_id, f'Your note with the header \"{data[0]}\" has been added')
         else:
             bot.send_message(chat_id, f'Note with the header \"{data[0]}\" exists')
@@ -50,14 +50,14 @@ def add_note(message):
 
 @bot.message_handler(commands=['get'])
 def get_notes(message):
+    SQLighter = dbWorker.SQLighter(os.getenv('DB_PATH'))
     bot.reply_to(message, 'Your notes:')
-    conn = dbWorker.connect(os.getenv('DB_PATH'))
     chat_id = message.chat.id
-    data = dbWorker.get(conn.cursor(), 'chat_id', chat_id)
+    data = SQLighter.get('chat_id', chat_id)
     for note in data:
         msg = note_template(note)
         bot.send_message(chat_id, msg, parse_mode='HTML')
-    conn.close()
+    SQLighter.close()
     print('Closing connection is successful')
 
 
