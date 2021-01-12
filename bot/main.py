f9b28eedf70489c404309c0f01418cfe19e33800
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-12 17:56:17 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,40 +1,13 @@
 from bot import dbWorker
 import os
 import time
-from datetime import datetime
+from utils import *
 import telebot
 from telebot import types
 from dotenv import load_dotenv
 
 load_dotenv()
-# ------------ Program variable start ----------- #
 bot = telebot.TeleBot(os.getenv("TOKEN"))
-status_codes = {
-    0: {'str': 'unready', 'reverse_str': 'ready', 'int': 1},
-    1: {'str': 'ready', 'reverse_str': 'unready', 'int': 0},
-}
-note_fields = ['header', 'text', 'time']
-# ------------ Program variables end ------------ #
-
-
-# ------------ Program functions start ---------- #
-def note_template(data):
-    return f"""
-<strong>Header</strong>: <i>{data[1]}</i>
-<strong>Text</strong>: <i>{data[2]}</i>
-<strong>Status</strong>: <i>{status_codes[data[3]].get('str')}</i>
-<strong>Due time</strong>: <i>{data[4]}</i>
-"""
-
-
-def check_time(date_time_str):
-    try:
-        datetime.strptime(date_time_str, '%d/%m/%y %H:%M:%S')
-        return True
-    except ValueError as e:
-        print(f'Error: {e}')
-        return False
-# ------------ Program functions end ------------ #
 
 
 # ------------ Bot functions start ---------- #
@@ -59,7 +32,8 @@ def add_note(message):
                 bot.send_message(
                     chat_id, f'Your note with the header \"{data[0]}\" has been added')
             else:
-                bot.send_message(chat_id, '<strong>Error!</strong> Enter the <i>datetime</i> field correctly', parse_mode='HTML')
+                bot.send_message(chat_id, '<strong>Error!</strong> Enter the <i>datetime</i> field correctly',
+                                 parse_mode='HTML')
         else:
             bot.send_message(
                 chat_id, f'Note with the header \"{data[0]}\" exists')
@@ -84,7 +58,8 @@ def edit_note(message):
                     f'Your note with new header \"<strong>{data[1]}</strong>\" has been updated',
                     parse_mode='HTML')
             else:
-                bot.send_message(chat_id, '<strong>Error!</strong> Enter the <i>datetime</i> field correctly', parse_mode='HTML')
+                bot.send_message(chat_id, '<strong>Error!</strong> Enter the <i>datetime</i> field correctly',
+                                 parse_mode='HTML')
         else:
             bot.send_message(
                 chat_id,
